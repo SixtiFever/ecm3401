b03fc5fd63498cb62b7aba1cb7de2be689659fc8
Jason <jdswift94@gmail.com>
2024-01-25 17:38:25 +0000

    Upon first scan, adding card to user document, adding customer to cafe document

--- components/user_general/Scanner.js
+++ components/user_general/Scanner.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import { AutoFocus, Camera, CameraType, CameraView } from 'expo-camera';
 import { firestore, auth } from '../../firebaseConfig';
-import { getDoc, collection, doc } from 'firebase/firestore';
+import { getDoc, collection, doc, setDoc, updateDoc } from 'firebase/firestore';
 
 const Scanner = ({navigation}) => {
     const [type, setType] = useState(CameraType.back);
@@ -31,7 +31,11 @@ const Scanner = ({navigation}) => {
             // updateLoyaltyCard
         } else {
             console.log('User hasn\'t visited cafe before');
-            // createLoyaltyCard
+
+
+            createCustomerFieldInCafeDoc(data);
+            createLoyaltyCardInUserDoc(data)
+
         }
         
         alert(`${data} has been scanned!`);
@@ -65,15 +69,79 @@ const Scanner = ({navigation}) => {
 @param1 -> QR Code data
 Checks whether the user currently has a virtual loyalty card for that cafe
 */
-async function hasUserVisitedCafeBefore(data) {
-    const cRef = collection(firestore, 'users')
-    const dRef = doc(cRef, auth.currentUser.email);
+async function hasUserVisitedCafeBefore(cafeEmail) {
+    const cRef = collection(firestore, 'cafes')
+    const dRef = doc(cRef, cafeEmail);
     const snap = await getDoc(dRef);
-    if ( snap.data()['cards'][data] != undefined ) {
+    if ( snap.data()['customers'][auth.currentUser.email] != undefined ) {
         return true;
     } else {
         return false;
     }
+}
+
+
+/*
+manage users  loyalty cards from the cafes customers field
+- Enables users card for that cafe to listen to the cafe document 
+and make dynamic changers on the cafes promotion changes.
+*/
+function createCustomerFieldInCafeDoc(cafeEmail) {
+    const cRef = collection(firestore, 'cafes');
+    const dRef = doc(cRef, cafeEmail);
+    const userEmail = auth.currentUser.email;
+    updateDoc(dRef, { customers: { [userEmail] : { 'current': 0, 'redeems': 0, 'loyaltyPoints': 5, 'rank':0 } }} ).then(() => {
+        console.log('Created customer instances for ' + auth.currentUser.email + ' in document for ' + cafeEmail);
+    }).catch(err => {
+        console.log('<Scanner.js> Error creating customer instances for cafe: ' + err);
+    })
+}
+
+/*
+@param1 -> cafe email
+updates the cafe.customers field with a new instances of the user
+*/
+function createLoyaltyCardInUserDoc(cafeEmail){
+    const cRef = collection(firestore, 'users');
+    const dRef = doc(cRef, auth.currentUser.email);
+    getCafeData(cafeEmail).then(snap => {
+        const card = generateLoyaltyCard(snap.data());
+        updateDoc(dRef, { cards :  card } );
+    }).catch( err => {
+        console.log('<Scanner.js> Error creating cafe loyalty card in user document');
+        return null;
+    })
+}
+
+/*
+@Param1 -> cafe email
+returns a snap of the cafe document
+*/
+async function getCafeData(scanData) {
+    const cRef = collection(firestore, 'cafes');
+    const dRef = doc(cRef, scanData);
+    const snap = await getDoc(dRef);
+    if ( snap.exists() ) {
+        return snap;
+    }
+}
+
+/*
+@Param1 -> Snapshot of cafe document
+Returns object consisting of contents for the users digital loyalty card.
+- Email (key), name, currentPromotion.reward, scansNeeded, postcode, currentScans
+*/
+function generateLoyaltyCard(cafeSnap){
+
+    const card = {
+        'cafeEmail': cafeSnap['cafeEmail'],
+        'cafeName': cafeSnap['cafeName'],
+        'reward': cafeSnap.currentPromotion.reward,
+        'scansNeeded': cafeSnap.currentPromotion.scansNeeded,
+        'postcode': cafeSnap.postcode,
+        'currentScans': 0,
+    }
+    return { [cafeSnap.cafeEmail]: card };
 }
 
 
